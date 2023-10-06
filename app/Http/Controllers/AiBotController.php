588e01954fe13f81be6046253113b6dd0cb7e5b8
Faraz <farazmunj@gmail.com>
2023-10-07 01:06:54 +0500

    working chat summary using Chat api

--- app/Http/Controllers/AiBotController.php
+++ app/Http/Controllers/AiBotController.php
@@ -14,11 +14,32 @@ class AiBotController extends Controller
 
     public function __invoke(Request $request)
     {
-        $input = $request->get('p', 'PHP is');
-        $result = $this->client->completions()->create([
-            'model' => 'gpt-3.5-turbo-instruct',
-            'prompt' => $input,
+        $transcript = $request->get('transcript', 'PHP is');
+        $response = $this->client->chat()->create([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                ['role' => 'system', 'content' => 'You are a helpful assistant that summarizes text.'],
+                ['role' => 'user', 'content' => $transcript],
+            ],
+
         ]);
-        return $result['choices'][0]['text'];
+        return $response->toArray();
+        // $response->id; // 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq'
+        // $response->object; // 'chat.completion'
+        // $response->created; // 1677701073
+        // $response->model; // 'gpt-3.5-turbo-0301'
+
+        // foreach ($response->choices as $result) {
+        //     $result->index; // 0
+        //     $result->message->role; // 'assistant'
+        //     $result->message->content; // '\n\nHello there! How can I assist you today?'
+        //     $result->finishReason; // 'stop'
+        // }
+
+        // $response->usage->promptTokens; // 9,
+        // $response->usage->completionTokens; // 12,
+        // $response->usage->totalTokens; // 21
+
+        // $response->toArray(); // ['id' => 'chatcmpl-6pMyfj1HF4QXnfvjtfzvufZSQq6Eq', ...]
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/ai-bot', AiBotController::class);
+Route::post('/ai-bot', AiBotController::class);
